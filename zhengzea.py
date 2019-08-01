e5227cbeb75ae38ab36f26574b7400b81157e4cb
2513953126@qq.com <2513953126@qq.com>
2019-08-01 18:02:27 +0800

    01

---
+++ zhengzea.py
@@ -0,0 +1,9 @@
+import re
+words = ['gold',' Google','Sogu','Guess']
+
+part = re.compile(r'.*g[^u]')#‘.’匹配任意一个字符，'*'匹配0个或多个字符，g后面不是u
+
+for w in words:
+    m = re.match(part,w)
+    if m:
+        print(w)
